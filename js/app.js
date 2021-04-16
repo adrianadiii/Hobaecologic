bd4cc54b240b0c1df6428228376bf50c9ae30b40
adrianadiii <adrian_adi47@yahoo.com>
2021-04-16 16:39:56 +0300

    16 aprilie

--- js/app.js
+++ js/app.js
@@ -123,3 +123,6 @@ function cereoferta() {
     x.style.display = "none";
   }
 }
+
+
+
--- produse.php
+++ produse.php
@@ -11,12 +11,18 @@
         if(isset($_GET['cat'])){
             $cat_id= $_GET['cat'];
             global $con;
-            $per_page= 15;
+            
             if(isset($_GET['page'])){
                 $page= $_GET['page'];
             } else {
                 $page= 1;
             }
+            $per_page= 15;
+            $offset = ($page-1) * $per_page;
+            $previous_page = $page - 1;
+            $next_page = $page + 1;
+            $adjacents = "2"; 
+
             $start_from= ($page- 1) * $per_page;
             $query = "SELECT * FROM produse where cat_id='$cat_id' order by product_id ASC LIMIT $start_from, $per_page";
             $result= mysqli_query($con, $query);
@@ -105,80 +111,214 @@
                 ";
             }
 
-?>
-</div>
-<div class="pagination">
-<?php 
-     $query = "select * from produse where cat_id='$cat_id'";
-     $result= mysqli_query($con, $query);
-     $total_records= mysqli_num_rows($result);
-     $total_pages= ceil($total_records/ $per_page);
+        ?>
+        </div>
+        <div class="pagination">
+        <?php 
+        $query = "select * from produse where cat_id='$cat_id'";
+        $result= mysqli_query($con, $query);
+        $total_records= mysqli_num_rows($result);
+        $total_pages= ceil($total_records/ $per_page);
+        $second_last = $total_pages - 1; // total page minus 1
+        ?>
 
-     if($total_pages == 0){
-         echo " ";
-     } elseif ($total_pages == 1){
-         echo " ";
-     } else {
+        <?php  if($page > 1){ echo "<li><a href='produse.php?cat=$cat_id&page=1'> << </a></li>"; } ?>
+        
+        <li <?php if($page <= 1){ echo "class='disabled'"; } ?>>
+        <?php if($page > 1){ 
+                echo "<a href='produse.php?cat=$cat_id&page=$previous_page'> < </a>";
+            }elseif ($page <1){
+                echo "";
+            }
+        ?>
+        </li>
+        
+        <?php 
+        
 
-        //  echo "
-                         
-        //      <li>
-             
-        //          <a href='produse.php?cat=$cat_id&page=1'> ".'First Page'." </a>
-             
-        //      </li>
-         
-        //  ";
-        if($page > 4){
-            echo "
-            <li>
-             
-                <a href='produse.php?cat=$cat_id&page=1'> ".'Prima Pagina'." </a>
-             
-            </li>
-            
-        
-        ";  
-         }
-                 
-         for($i=1; $i<=$total_pages; $i++){
-             
-                 echo "
-         
-             <li>
-             
-                 <a href='produse.php?cat=$cat_id&page=".$i."'> ".$i." </a>
-             
-             </li>
-         
-         ";  
-             
-         };
-         
-         if($page > 4){
-            echo "
-            <li>
-             
-                <a href='produse.php?cat=$cat_id&page=$total_pages'> ".'Ultima Pagina'." </a>
-             
-            </li>
-            
-        
-        ";  
-         }       
-        //  echo "
-         
-        //      <li>
-             
-        //          <a href='produse.php?cat=$cat_id&page=$total_pages'> ".'Last Page'." </a>
-             
-        //      </li>
-         
-        //  ";
-     }
- }
-}
+
+        if ($total_pages <= 10){  	 
+            for ($counter = 1; $counter <= $total_pages; $counter++){
+                if ($counter == $page) {
+                    echo "<li class='active'><a>$counter</a></li>";	
+                    }else{
+                        echo "<li>
+                            <a href='produse.php?cat=$cat_id&page=$counter'>$counter</a>
+                        </li>";
+                    }
+            }
+        }
+        elseif($total_pages > 10){
+		
+            if($page <= 4) {			
+                for ($counter = 1; $counter < 8; $counter++){		 
+                        if ($counter == $page) {
+                    echo "<li class='active'>
+                            <a>$counter</a>
+                        </li>";	
+                    }else{
+                        echo "<li>
+                                <a href='produse.php?cat=$cat_id&page=$counter'>$counter</a>
+                            </li>";
+                    }
+                }
+                    echo "<li>
+                            <a>...</a>
+                        </li>";
+                    echo "<li>
+                            <a href='produse.php?cat=$cat_id&page=$second_last'>$second_last</a>
+                        </li>";
+                    echo "<li>
+                            <a href='produse.php?cat=$cat_id&page=$total_pages'>$total_pages</a>
+                        </li>";
+	        }
+
+            elseif($page > 4 && $page < $total_pages - 4) {		 
+                echo "<li>
+                        <a href='?page_no=1'>1</a>
+                    </li>";
+                echo "<li>
+                        <a href='?page_no=2'>2</a>
+                    </li>";
+                echo "<li>
+                        <a>...</a>
+                    </li>";
+                for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++) {			
+                    if ($counter == $page) {
+                        echo "<li class='active'>
+                                <a>$counter</a>
+                            </li>";	
+                    }else{
+                        echo "<li>
+                                <a href='produse.php?cat=$cat_id&page=$counter'>$counter</a>
+                            </li>";
+                    }                  
+                }
+                echo "<li>
+                        <a>...</a>
+                    </li>";
+                echo "<li>
+                        <a href='produse.php?cat=$cat_id&page=$second_last'>$second_last</a>
+                    </li>";
+                echo "<li>
+                        <a href='produse.php?cat=$cat_id&page=$total_pages'>$total_pages</a>
+                    </li>";      
+            }
+		
+		    else {
+                echo "<li>
+                        <a href='produse.php?cat=$cat_id&page=1'>1</a>
+                    </li>";
+		        echo "<li>
+                        <a href='produse.php?cat=$cat_id&page=2'>2</a>
+                    </li>";
+                echo "<li>
+                        <a>...</a>
+                    </li>";
+
+                for ($counter = $total_pages - 6; $counter <= $total_pages; $counter++) {
+                    if ($counter == $page) {
+		                echo "<li class='active'>
+                                <a>$counter</a>
+                            </li>";	
+				    }else{
+                        echo "<li>
+                                <a href='produse.php?cat=$cat_id&page=$counter'>$counter</a>
+                            </li>";
+				    }                   
+                }
+            }
+	    }
+        ?>
+    
+        <li <?php if($page >= $total_pages){ echo "class='disabled'"; } ?>>
+        <?php if($page < $total_pages) { 
+                echo "<a href='produse.php?cat=$cat_id&page=$next_page'> > </a"; 
+            }elseif($page = $total_pages){
+                echo "";
+            } ?>
+        </li>
+        <?php if($page < $total_pages){
+            echo "<li>
+                    <a href='produse.php?cat=$cat_id&page=$total_pages'> >> </a>
+                </li>";
+        }
+
+    }
+} 
+
 ?>
+
+
+<?php
+
+        // if($total_pages == 0){
+        //     echo " ";
+        // } elseif ($total_pages == 1){
+        //     echo " ";
+        // } else {
+
+        //     //  echo "
+                            
+        //     //      <li>
+                
+        //     //          <a href='produse.php?cat=$cat_id&page=1'> ".'First Page'." </a>
+                
+        //     //      </li>
+            
+        //     //  ";
+        //     if($page > 4){
+        //         echo "
+        //         <li>
+        //             <a href='produse.php?cat=$cat_id&page=1'> ".'Prima Pagina'." </a>
+        //         </li>
+        //     ";  
+        //     }
+
+        //     if($page>2){
+        //         echo "
+        //         <li>
+        //             <a href='produse.php?cat=$cat_id&page=1'> ".'1'." </a>
+        //         </li>
+        //         ";
+        //         if($page >3){
+        //             echo "
+        //             <li>
+        //                 <span> ".'...'." </span>
+        //             </li>
+        //         ";
+        //         }
+        //     }
+                    
+        //     for($i=1; $i<=$total_pages; $i++){
+                
+        //         echo "
+        //         <li>
+        //             <a href='produse.php?cat=$cat_id&page=".$i."'> ".$i." </a>
+        //         </li>
+        //     ";   
+        //     }
+            
+        //     if($page > 4){
+        //         echo "
+        //         <li> 
+        //             <a href='produse.php?cat=$cat_id&page=$total_pages'> ".'Ultima Pagina'." </a>
+        //         </li>        
+        //     ";  
+        //     }       
+        //     //  echo "
+            
+        //     //      <li>
+                
+        //     //          <a href='produse.php?cat=$cat_id&page=$total_pages'> ".'Last Page'." </a>
+                
+        //     //      </li>
+            
+        //     //  ";
+        // }
+
+?>
+
 </div>
 
 <?php include 'footer.php' ?>
